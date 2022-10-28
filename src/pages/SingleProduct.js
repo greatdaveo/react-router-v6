e484c2fb4ef7e3f3ee797286dee090ff917012fa
David Olowomeye <100958002+greatdaveo@users.noreply.github.com>
2022-10-28 03:44:19 +0100

    Add files via upload

---
+++ src/pages/SingleProduct.js
@@ -0,0 +1,15 @@
+import { Link, useParams } from "react-router-dom";
+
+const SingleProduct = () => {
+
+  const { productId } = useParams()
+  
+  return (
+    <section className='section product'>
+      <h4>{productId}</h4>
+      <Link to='products'>Back to products</Link> 
+    </section>
+  );
+};
+
+export default SingleProduct;
